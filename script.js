327fb3b5e9c65b412a46c3a282fdf98a71881319
Gabriel Costa <gabrielncosta208@gmail.com>
2019-07-31 21:27:01 -0300

    addtask

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Define UI Vars
- const form = document.querySelector('#task-form');
+const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('clear-tasks');
+const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
@@ -14,7 +14,7 @@ function loadEventListeners() {
 
 function addTask(e) {
     if(taskInput.value === '') {
-        alert('Add a task')
+        alert('Add a task');
     }
     // Create li element
     const li = document.createElement('li');
@@ -24,13 +24,20 @@ function addTask(e) {
     li.appendChild(document.createTextNode(taskInput.value));
     // Create new link element
     const link = document.createElement('a');
+    
     // add Class
     link.className = 'delete-item secondary-content';
     // add icon html
-    link.innerHTML = '<i class="fa fa-remove></i> '
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+
+
+
     // Append the link to li
     li.appendChild(link);
+    // Append li to ul
+    taskList.appendChild(li);
+    // Clear input
+    taskInput.value = '';
     
-
     e.preventDefault();
 }
