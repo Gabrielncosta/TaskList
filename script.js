dab89b1553aab76d5d082a38efca94b8ed70fad6
Gabriel Costa <gabrielncosta208@gmail.com>
2019-07-31 21:01:00 -0300

    EventListeners

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 // Define UI Vars
-const form = document.querySelector('#task-form');
+ const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
 loadEventListeners();
+
+function loadEventListeners() {
+    // Add task event
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('Add a task')
+    }
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a');
+    // add Class
+    link.className = 'delete-item secondary-content';
+
+    e.preventDefault();
+}
